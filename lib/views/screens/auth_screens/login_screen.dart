bb65cac10c6d13d913529645750f016f79e2cdd1
adogbeji <baka_1993@yahoo.co.uk>
2024-04-09 20:47:16 +0100

    Set mainAxisAlignment property of Column in login screen

--- lib/views/screens/auth_screens/login_screen.dart
+++ lib/views/screens/auth_screens/login_screen.dart
@@ -6,10 +6,13 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        children: [
-          Text('Log In'),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Log In'),
+          ],
+        ),
       ),
     );
   }
