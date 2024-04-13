6c98d7560039609506928a45e559a74cb7b8368e
adogbeji <baka_1993@yahoo.co.uk>
2024-04-13 20:29:24 +0100

    Add image and more text to login screen

--- lib/views/screens/auth_screens/login_screen.dart
+++ lib/views/screens/auth_screens/login_screen.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Log In', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,),),
+            // Text(
+            //   'Log In',
+            //   style: GoogleFonts.getFont(
+            //     'Lato',
+            //     fontSize: 23,
+            //     color: Colors.black,
+            //     fontWeight: FontWeight.bold,
+            //     letterSpacing: 0.2,
+            //   ),
+            // ),
+
+            const Text(
+              'Log In',
+              style: TextStyle(
+                fontSize: 23,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.2,
+              ),
+            ),
+
+            const Text('To explore the world exclusives', style: TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+              letterSpacing: 0.2,
+            ),),
+
+            Image.asset('assets/images/padlock.jpg', width: 200, height: 200,),
           ],
         ),
       ),
