d6edac4ec3dda51026a8b8bbe63c066cb206bdd8
adogbeji <baka_1993@yahoo.co.uk>
2024-04-15 12:01:42 +0100

    Downgrade google_fonts package & add more styling for login screen

--- lib/views/screens/auth_screens/login_screen.dart
+++ lib/views/screens/auth_screens/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -11,34 +11,37 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Text(
-            //   'Log In',
-            //   style: GoogleFonts.getFont(
-            //     'Lato',
-            //     fontSize: 23,
-            //     color: Colors.black,
-            //     fontWeight: FontWeight.bold,
-            //     letterSpacing: 0.2,
-            //   ),
-            // ),
-
-            const Text(
+            Text(
               'Log In',
-              style: TextStyle(
+              style: GoogleFonts.getFont(
+                'Lato',
                 fontSize: 23,
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
                 letterSpacing: 0.2,
               ),
             ),
-
-            const Text('To explore the world exclusives', style: TextStyle(
-              fontSize: 14,
-              color: Colors.black,
-              letterSpacing: 0.2,
-            ),),
-
-            Image.asset('assets/images/Illustration.png', width: 200, height: 200,),
+            Text(
+              'To explore the world exclusives',
+              style: GoogleFonts.getFont(
+                'Lato',
+                fontSize: 14,
+                color: Colors.black,
+                letterSpacing: 0.2,
+              ),
+            ),
+            Image.asset(
+              'assets/images/Illustration.png',
+              width: 200,
+              height: 200,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Email',
+                style: GoogleFonts.getFont('Nunito Sans'),
+              ),
+            ),
           ],
         ),
       ),
