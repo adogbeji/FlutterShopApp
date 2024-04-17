17cab5b212039f30d9a0e37fe2f2da5317da59aa
adogbeji <baka_1993@yahoo.co.uk>
2024-04-17 12:13:43 +0100

    Add pasword input field, change background colour of Scaffold & apply padding to loign screen

--- lib/views/screens/auth_screens/login_screen.dart
+++ lib/views/screens/auth_screens/login_screen.dart
@@ -7,71 +7,111 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Log In',
-              style: GoogleFonts.getFont(
-                'Lato',
-                fontSize: 23,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.2,
-              ),
-            ),
-            Text(
-              'To explore the world exclusives',
-              style: GoogleFonts.getFont(
-                'Lato',
-                fontSize: 14,
-                color: Colors.black,
-                letterSpacing: 0.2,
-              ),
-            ),
-            Image.asset(
-              'assets/images/Illustration.png',
-              width: 200,
-              height: 200,
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Email',
+      backgroundColor: Colors.white.withOpacity(0.95,),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Log In',
                 style: GoogleFonts.getFont(
-                  'Nunito Sans',
-                  fontWeight: FontWeight.w600,
+                  'Lato',
+                  fontSize: 23,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
                   letterSpacing: 0.2,
                 ),
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(9),
-                ),
-                focusedBorder: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                labelText: 'enter your email',
-                labelStyle: GoogleFonts.getFont(
-                  'Nunito Sans',
+              Text(
+                'To explore the world exclusives',
+                style: GoogleFonts.getFont(
+                  'Lato',
                   fontSize: 14,
-                  letterSpacing: 0.1,
+                  color: Colors.black,
+                  letterSpacing: 0.2,
                 ),
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Image.asset(
-                    'assets/icons/email.png',
-                    width: 20,
-                    height: 20,
+              ),
+              Image.asset(
+                'assets/images/Illustration.png',
+                width: 200,
+                height: 200,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Email',
+                  style: GoogleFonts.getFont(
+                    'Nunito Sans',
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0.2,
                   ),
                 ),
               ),
-            ),
-          ],
+        
+              // EMAIL INPUT FIELD
+              TextFormField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  labelText: 'enter your email',
+                  labelStyle: GoogleFonts.getFont(
+                    'Nunito Sans',
+                    fontSize: 14,
+                    letterSpacing: 0.1,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'assets/icons/email.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                ),
+              ),
+        
+              const SizedBox(
+                height: 20,
+              ),
+        
+              // PASSWORD INPUT FIELD
+              TextFormField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  labelText: 'enter your password',
+                  labelStyle: GoogleFonts.getFont(
+                    'Nunito Sans',
+                    fontSize: 14,
+                    letterSpacing: 0.1,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'assets/icons/password.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  suffixIcon: const Icon(
+                    Icons.visibility,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
